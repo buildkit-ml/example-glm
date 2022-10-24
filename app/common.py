bb4a152a3f602fa3ce2d7136124c29519ac7a8db
Xiaozhe Yao <askxzyao@gmail.com>
2022-10-24 14:15:58 +0200

    try glm

--- app/common.py
+++ app/common.py
@@ -10,11 +10,14 @@ import torch.distributed as dist
 class FastInferenceInterface:
     def __init__(self, model_name: str, args=None) -> None:
         self.model_name = model_name
+        self.model_id = args.model_id
+        self.rank = args.rank
 
     def infer(self, job_id, args) -> Dict:
         pass
 
     async def on_message(self, msg):
+        self.nc.publish(self.model_name+f".{self.model_id}", "START")
         instruction = json.loads(msg.data.decode("utf-8"))
         instruction['args'] = json.loads(instruction['args'])
         try:
@@ -57,14 +60,28 @@ class FastInferenceInterface:
     def on_open(self, ws):
         ws.send(f"JOIN:{self.model_name}")
 
+    async def on_secondary_message(self, msg):
+        if msg == 'START':
+            self.infer(None, None)
+        else:
+            raise Exception("Unknown message")
+
     def start(self):
         my_rank = dist.get_rank()
         nats_url = os.environ.get("NATS_URL", "localhost:8092/my_coord")+'.'+str(my_rank)
-        async def listen():
-            nc = await nats.connect(f"nats://{nats_url}")
-            sub = await nc.subscribe(subject=self.model_name, queue=self.model_name, cb=self.on_message)
-        loop = asyncio.get_event_loop()
-        future = asyncio.Future()
-        asyncio.ensure_future(listen())
-        loop.run_forever()
-
+        if my_rank == 0:
+            async def listen():
+                self.nc = await nats.connect(f"nats://{nats_url}")
+                sub = await self.nc.subscribe(subject=self.model_name, queue=self.model_name, cb=self.on_message)
+            loop = asyncio.get_event_loop()
+            future = asyncio.Future()
+            asyncio.ensure_future(listen())
+            loop.run_forever()
+        else:
+            async def listen():
+                self.nc = await nats.connect(f"nats://{nats_url}")
+                sub = await self.nc.subscribe(subject=self.model_name+f".{self.model_id}", cb=self.on_secondary_message)
+            loop = asyncio.get_event_loop()
+            future = asyncio.Future()
+            asyncio.ensure_future(listen())
+            loop.run_forever()
